4efb20be1c804c8ea38896daae772ab2faffc0ca
Jinoh Kim <kjo8024@naver.com>
2016-07-10 18:39:06 +0900

    메인 액티비티 약간

---
+++ WearAlarm/mobile/src/main/java/com/wolgot/jinoh/wearalarm/MainActivity.java
@@ -0,0 +1,29 @@
+package com.wolgot.jinoh.wearalarm;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Toolbar set
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        collapsingToolbarLayout.setTitle("");
+    }
+}
